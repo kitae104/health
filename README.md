2f19b01b6dcec015c23e92f8747bb897c1426e30
Kitae's Github <aqua0405@gmail.com>
2025-12-10 23:34:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# health
+Health Care 스프링 백엔드
---
+++ src/test/java/kitae/spring/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package kitae.spring.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
