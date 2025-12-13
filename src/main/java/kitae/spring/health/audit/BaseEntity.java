32af5511bcb8938f02a2a1e87cdb445485d17127
Kitae's Github <aqua0405@gmail.com>
2025-12-13 22:51:24 +0900

    Audit 기능

---
+++ src/main/java/kitae/spring/health/audit/BaseEntity.java
@@ -0,0 +1,24 @@
+package kitae.spring.health.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass                       // 테이블로 매핑하지 않고, 자식 Entity에게 매핑 정보를 상속하기 위한 어노테이션
+@Getter
+@Setter
+public class BaseEntity extends BaseTimeEntity {
+  @CreatedBy                      // 생성자
+  @Column(updatable = false, length = 30)      // 수정 불가
+  private String createdBy;       // 등록자
+
+  @LastModifiedBy                 // 최종 수정자
+  @Column(length = 30)
+  private String modifiedBy;      // 수정자
+}
---
+++ src/main/java/kitae/spring/health/audit/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package kitae.spring.health.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass                       // 테이블로 매핑하지 않고, 자식 Entity에게 매핑 정보를 상속하기 위한 어노테이션
+@Getter
+@Setter
+public class BaseTimeEntity {
+  @CreatedDate                        // 최초 생성 시점
+  @Column(updatable = false)          // 수정 불가
+  private LocalDateTime createdAt;      // 등록일
+
+  @LastModifiedDate                   // 최종 수정 시점
+  private LocalDateTime updatedAt;      // 수정일
+}
