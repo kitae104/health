47f8b9cb14897665918a6444937fa888e38ff5cf
Kitae's Github <aqua0405@gmail.com>
2025-12-13 22:51:56 +0900

    Create User.java

---
+++ src/main/java/kitae/spring/health/users/entity/User.java
@@ -0,0 +1,41 @@
+package kitae.spring.health.users.entity;
+
+import jakarta.persistence.*;
+import kitae.spring.health.role.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profilePictureUrl;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
+}
