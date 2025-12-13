8629008f334d4a17b835f3f8f262056c1c170a1e
Kitae's Github <aqua0405@gmail.com>
2025-12-13 22:52:01 +0900

    Create UserRepository.java

---
+++ src/main/java/kitae/spring/health/users/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package kitae.spring.health.users.repository;
+
+import kitae.spring.health.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
