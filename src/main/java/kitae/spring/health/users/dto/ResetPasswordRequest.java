e348ef9cdda9d0837f76325fb71fab029069f2c5
Kitae's Github <aqua0405@gmail.com>
2025-12-15 01:34:47 +0900

    Create ResetPasswordRequest.java

---
+++ src/main/java/kitae/spring/health/users/dto/ResetPasswordRequest.java
@@ -0,0 +1,15 @@
+package kitae.spring.health.users.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResetPasswordRequest {
+
+    private String email;
+
+    private String code;
+
+    private String newPassword;
+}
