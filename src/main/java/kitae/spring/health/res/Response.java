c61fbe76cbede3ba31873608bf3c15352b2f0b6c
Kitae's Github <aqua0405@gmail.com>
2025-12-15 12:56:49 +0900

    Create Response.java

---
+++ src/main/java/kitae/spring/health/res/Response.java
@@ -0,0 +1,17 @@
+package kitae.spring.health.res;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+
+    private int statusCode; // 상태 코드
+
+    private String message; // 응답 메시지
+
+    private T data; // 응답 데이터
+}
