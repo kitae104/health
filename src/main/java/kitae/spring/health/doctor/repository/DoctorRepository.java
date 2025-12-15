50fb06553a9d7265ed6216cef80b27a143022637
Kitae's Github <aqua0405@gmail.com>
2025-12-15 12:56:37 +0900

    Create DoctorRepository.java

---
+++ src/main/java/kitae/spring/health/doctor/repository/DoctorRepository.java
@@ -0,0 +1,16 @@
+package kitae.spring.health.doctor.repository;
+
+import kitae.spring.health.doctor.entity.Doctor;
+import kitae.spring.health.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+
+    Optional<Doctor> findByUser(User user);
+
+    // 특정 분야의 의사 목록을 반환하는 메서드
+    List<Doctor> findBySpecialization(String specialization);
+}
