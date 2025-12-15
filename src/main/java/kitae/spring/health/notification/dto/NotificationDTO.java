bcec3adb16fec952ba912525a97bbd55a8b8f9cf
Kitae's Github <aqua0405@gmail.com>
2025-12-15 12:56:39 +0900

    Create NotificationDTO.java

---
+++ src/main/java/kitae/spring/health/notification/dto/NotificationDTO.java
@@ -0,0 +1,32 @@
+package kitae.spring.health.notification.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import kitae.spring.health.enums.NotificationType;
+import kitae.spring.health.users.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class NotificationDTO {
+
+    private Long id;    // 아이디
+
+    private String subject; // 제목
+
+    @NotBlank(message = "Recipient is required")
+    private String recipient;   // 수신자
+
+    private String message; // 메시지 내용
+
+    private NotificationType type; // 알림 유형
+
+    private LocalDateTime createdAt; // 생성 일시
+
+    private String templateName; // 템플릿 이름
+
+    private Map<String , Object> templateVariables; // 템플릿 변수
+}
