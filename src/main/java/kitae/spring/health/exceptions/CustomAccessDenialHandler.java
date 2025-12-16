c65d4fa3cb7c98b984d77dc10a3e527ea98aebdb
Kitae's Github <aqua0405@gmail.com>
2025-12-17 01:32:47 +0900

    Create CustomAccessDenialHandler.java

---
+++ src/main/java/kitae/spring/health/exceptions/CustomAccessDenialHandler.java
@@ -0,0 +1,36 @@
+package kitae.spring.health.exceptions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kitae.spring.health.res.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDenialHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        Response<?> errorResponse = Response.builder()
+                .statusCode(HttpStatus.FORBIDDEN.value()) //403. valid jwt. but user not permitted to access the route
+                .message(accessDeniedException.getMessage())
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
