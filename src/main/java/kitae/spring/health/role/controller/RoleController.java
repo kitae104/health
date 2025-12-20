0ee56e55d987114c0365eb65c63eb3dc9aaf9d95
Kitae's Github <aqua0405@gmail.com>
2025-12-20 23:40:09 +0900

    Create RoleController.java

---
+++ src/main/java/kitae/spring/health/role/controller/RoleController.java
@@ -0,0 +1,40 @@
+package kitae.spring.health.role.controller;
+
+import kitae.spring.health.response.Response;
+import kitae.spring.health.role.entity.Role;
+import kitae.spring.health.role.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
+@RequiredArgsConstructor
+@PreAuthorize("hasAuthority('ADMIN')")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @PostMapping
+    public ResponseEntity<Response<Role>> createRole(@RequestBody Role role){
+        return ResponseEntity.ok(roleService.createRole(role));
+    }
+
+    @PutMapping
+    public ResponseEntity<Response<Role>> updateRole(@RequestBody Role role){
+        return ResponseEntity.ok(roleService.updateRole(role));
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<List<Role>>> getAllRoles(){
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response<?>> deleteRole(@PathVariable Long id){
+        return ResponseEntity.ok(roleService.deleteRole(id));
+    }
+}
