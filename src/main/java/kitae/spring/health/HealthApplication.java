759c9145e54d8e0b913eebb684a71990815382a3
Kitae's Github <aqua0405@gmail.com>
2025-12-20 23:39:30 +0900

    Update HealthApplication.java

--- src/main/java/kitae/spring/health/HealthApplication.java
+++ src/main/java/kitae/spring/health/HealthApplication.java
@@ -2,8 +2,10 @@ package kitae.spring.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HealthApplication {
 
   public static void main(String[] args) {
