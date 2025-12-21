31a8e60caa88dbb2e0bca95aa73df975fd714c2c
Kitae's Github <aqua0405@gmail.com>
2025-12-21 16:50:54 +0900

    Update SwaggerConfig.java

--- src/main/java/kitae/spring/health/config/SwaggerConfig.java
+++ src/main/java/kitae/spring/health/config/SwaggerConfig.java
@@ -13,29 +13,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class SwaggerConfig {
 
-  public static final String BEARER_KEY = "BearerAuth";
+    public static final String BEARER_KEY = "BearerAuth";
 
-  @Bean
-  public OpenAPI openAPI() {
-    return new OpenAPI()
-        .components(new Components())
-        .info(apiInfo())
-        // JWT 보안 스키마 추가
-        .addSecurityItem(new SecurityRequirement().addList(BEARER_KEY))
-        .components(new io.swagger.v3.oas.models.Components()
-            .addSecuritySchemes(BEARER_KEY,
-                new SecurityScheme()
-                    .name(BEARER_KEY)
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT")
-                    .description("JWT Bearer 인증을 위한 헤더 입력 (예: 'Bearer {token}')")));
-  }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo())
+            // JWT 보안 스키마 추가
+            .addSecurityItem(new SecurityRequirement().addList(BEARER_KEY))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(BEARER_KEY,
+                    new SecurityScheme()
+                        .name(BEARER_KEY)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("JWT Bearer 인증을 위한 헤더 입력 (예: 'Bearer {token}')")));
+    }
 
-  private Info apiInfo() {
-    return new Info()
-        .title("My IMS API") // API의 제목
-        .description("Swagger UI") // API에 대한 설명
-        .version("1.0.0"); // API의 버전
-  }
+    private Info apiInfo() {
+        return new Info()
+            .title("Health Care API") // API의 제목
+            .description("Swagger UI") // API에 대한 설명
+            .version("1.0.0"); // API의 버전
+    }
 }
