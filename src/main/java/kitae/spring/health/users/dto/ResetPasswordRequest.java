5fc8b82a6cd69e53562a0f4d0c5e8dc3514dad39
Kitae's Github <aqua0405@gmail.com>
2025-12-23 01:40:42 +0900

    Update ResetPasswordRequest.java

--- src/main/java/kitae/spring/health/users/dto/ResetPasswordRequest.java
+++ src/main/java/kitae/spring/health/users/dto/ResetPasswordRequest.java
@@ -7,9 +7,9 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ResetPasswordRequest {
 
-    private String email;
+    private String email;   // 이메일
 
-    private String code;
+    private String code; // 인증 코드
 
-    private String newPassword;
+    private String newPassword; // 새 비밀번호
 }
