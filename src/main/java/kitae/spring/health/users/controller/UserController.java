f2360f80af54db2e7cdaf6b1d9948ba573e07f52
Kitae's Github <aqua0405@gmail.com>
2025-12-23 15:30:55 +0900

    Update UserController.java

--- src/main/java/kitae/spring/health/users/controller/UserController.java
+++ src/main/java/kitae/spring/health/users/controller/UserController.java
@@ -1,13 +1,17 @@
 package kitae.spring.health.users.controller;
 
+import jakarta.validation.Valid;
 import kitae.spring.health.response.Response;
+import kitae.spring.health.users.dto.UpdatePasswordRequest;
 import kitae.spring.health.users.dto.UserDTO;
 import kitae.spring.health.users.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +23,26 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<Response<UserDTO>> getMyUserDetails() {
         return ResponseEntity.ok(userService.getMyUserDetails());
+    }
+
+    @GetMapping("/by-id/{userId}")
+    public ResponseEntity<Response<UserDTO>> getUserById(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")  // ADMIN 권한이 있는 사용자만 접근 가능
+    public ResponseEntity<Response<List<UserDTO>>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<Response<?>> updatePassword(@RequestBody @Valid UpdatePasswordRequest updatePasswordRequest) {
+        return ResponseEntity.ok(userService.updatePassword(updatePasswordRequest));
+    }
+
+    @PutMapping("/profile-picture")
+    public ResponseEntity<Response<?>> updateProfilePicture(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(userService.uploadProfilePicture(file));
     }
 }
