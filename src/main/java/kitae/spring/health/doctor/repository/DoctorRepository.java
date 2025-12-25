98281c1206ddbc648a879f93eeb325862693bc32
Kitae's Github <aqua0405@gmail.com>
2025-12-25 19:36:27 +0900

    Update DoctorRepository.java

--- src/main/java/kitae/spring/health/doctor/repository/DoctorRepository.java
+++ src/main/java/kitae/spring/health/doctor/repository/DoctorRepository.java
@@ -1,6 +1,7 @@
 package kitae.spring.health.doctor.repository;
 
 import kitae.spring.health.doctor.entity.Doctor;
+import kitae.spring.health.enums.Specialization;
 import kitae.spring.health.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,5 +13,5 @@ public interface DoctorRepository extends JpaRepository<Doctor,Long> {
     Optional<Doctor> findByUser(User user);
 
     // 특정 분야의 의사 목록을 반환하는 메서드
-    List<Doctor> findBySpecialization(String specialization);
+    List<Doctor> findBySpecialization(Specialization specialization);
 }
